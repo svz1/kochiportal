e7c5d8cc3642f0ef76032c0849e712bb7dc2119a
Svz <saviosabu1@gmail.com>
2024-11-28 13:56:44 +0530

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navs'>
+        <nav>
+      <h1>AWS to Azure Migration Helper</h1>
+      <p>Crafted by savio.sabu@soti.net</p>
+      </nav>
+
+      </div>
+   
+  )
+}
+
+export default Navbar
+
